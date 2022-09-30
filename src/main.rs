81c6981ea599e20e66f0ff2f21dc9fc4f508dbdd
Ian Johnson <IJohnson@covermymeds.com>
2022-09-30 16:32:15 -0400

    Add directory serving and test support files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+#![deny(warnings)]
+
+#[tokio::main]
+async fn main() {
+    pretty_env_logger::init();
+
+    warp::serve(warp::fs::dir("tests/support"))
+        .run(([127, 0, 0, 1], 3030))
+        .await;
 }
