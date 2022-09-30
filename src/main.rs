a016d165211ec27037912a34efd67b2daa0b8531
Ian Johnson <IJohnson@covermymeds.com>
2022-09-30 17:04:31 -0400

    Add default config and serve the given dir

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,26 @@
 #![deny(warnings)]
+use serde_derive::{Serialize, Deserialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+struct FreightConfig {
+    dir: String,
+}
+
+impl ::std::default::Default for FreightConfig {
+    fn default() -> Self { Self { dir: "tests/support".into() } }
+}
 
 #[tokio::main]
 async fn main() {
     pretty_env_logger::init();
 
-    warp::serve(warp::fs::dir("tests/support"))
+    let cfg: FreightConfig = confy::load("freight", None).unwrap();
+    let file = confy::get_configuration_file_path("freight", None).unwrap();
+    println!("The configuration file path is: {:#?}", file);
+    println!("The configuration is:");
+    println!("{:#?}", cfg);
+
+    warp::serve(warp::fs::dir(cfg.dir))
         .run(([127, 0, 0, 1], 3030))
         .await;
 }
